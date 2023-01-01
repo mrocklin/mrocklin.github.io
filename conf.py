d18d797ca30133d5450da46a1d6ac7e57d21e178
Matthew Rocklin <mrocklin@gmail.com>
2022-12-31 19:46:59 -0800

    turn on show-prev-next

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ blog_post_pattern = "posts/*"
 post_redirect_refresh = 1
 post_auto_image = 1
 post_auto_excerpt = 1
-post_show_prev_next = False
+post_show_prev_next = True
 blog_feed_fulltext = True
 blog_authors = {
     "Matthew Rocklin": ("Matthew Rocklin", "https://matthewrocklin.com"),
